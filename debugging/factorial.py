bf8335b229c32a308edb67a0e17f652bd1fd003a
osamaghamdi <ghamdios235@gmail.com>
2025-05-06 10:36:31 +0300

    Update factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,16 +2,11 @@
 import sys
 
 def factorial(n):
-    if n < 0:
-        return "Undefined for negative numbers"
-    result = 1
+    result = 1:
     while n > 1:
         result *= n
         n -= 1
-    return result
+        return result
 
-if len(sys.argv) > 1:
-    f = factorial(int(sys.argv[1]))
-    print(f)
-else:
-    print("Please provide a number as an argument.")
+f = factorial(int(sys.argv[1]))
+print(f)
